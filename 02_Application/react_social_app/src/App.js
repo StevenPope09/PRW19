3e0d42674135eb0511c4f9b948672995b791d5ea
Steven Pope <sjpope@student.fullsail.edu>
2019-08-15 14:30:11 -0500

    added imports

--- 02_Application/react_social_app/src/App.js
+++ 02_Application/react_social_app/src/App.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import './App.css';
 import MyBtn from './components/buttons/MyBtn';
+import logo from '../src/images/logo.png';
+import MyForm from './components/myForm/MyForm';
+import Search from './components/search/Search';
 
 class App extends Component {
   render() {
@@ -9,7 +12,15 @@ class App extends Component {
         <MyBtn btnText="Home" />
         <MyBtn btnText="About" />
         <MyBtn btnText="Contact" />
+
+        <p>
+          <img src={logo} alt="Logo" />
+        </p>
+        <MyForm addName={this.addName} />
+        <Search searchMe={this.searchMe} />
+
       </div>
+      
     )
   }
 }
