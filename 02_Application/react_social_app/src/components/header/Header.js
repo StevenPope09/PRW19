079dead95dc58fc444a02f6ab75d5f3b13bcb457
Steven Pope <sjpope@student.fullsail.edu>
2019-08-17 13:25:39 -0500

    added react icons to header component

--- 02_Application/react_social_app/src/components/header/Header.js
+++ 02_Application/react_social_app/src/components/header/Header.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import Nav from '../nav/Nav'
 import logo from '../../images/logo.png'
+import { FaCog, FaBell } from 'react-icons/fa';
 
 const Header = props => {
     return (
         <header style={styles.header}>
             <img src={logo} alt='Logo' style={styles.logo} />
             <h1 style={styles.h1}>{props.pgTitle}</h1>
+            <span style={styles.headNot}>
+                <p style={styles.notify}><FaBell /> </p>
+                <p style={styles.notify}><FaCog /> </p>
+            </span>
 
             <nav style={styles.nav}>
                 <Nav />
             </nav>
         </header>
-        
+
     )
 }
 export default Header
@@ -24,6 +29,7 @@ const styles = {
         color: 'black',
         width: '100%',
         backgroundColor: '#279AF1',
+        fontFamily: 'Times New Roman, Times, serif'
 
     },
     logo: {
@@ -39,10 +45,22 @@ const styles = {
         width: '100%',
         marginLeft: '1.5%',
         marginBottom: '1.5%'
-        
+
     },
     h1: {
         marginLeft: '1.5%',
-        marginBottom: '3%'
+        //marginBottom: '3%'
+    },
+    notify: {
+        position: 'relative',
+        left: '95%',
+        top: '-180px',
+        display: 'inline',
+        marginRight: '10px',
+
+    },
+    headerNot: {
+        display: 'flex',
+        flexDirection: 'row'
     }
 }
