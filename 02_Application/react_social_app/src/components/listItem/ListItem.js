5a8018b44b243ba5973087201473d977619d987e
Steven Pope <sjpope@student.fullsail.edu>
2019-08-17 12:29:27 -0500

    list item example added

---
+++ 02_Application/react_social_app/src/components/listItem/ListItem.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const ListItem = props => {
+    return(
+        <li key={props.id} style={styles.list}>
+            <span>{props.val.gName}</span>
+            <span>{props.val.gPrice}</span>
+            <button onClick={props.delMe}>Delete</button>
+        </li>
+    )
+}
+
+export default ListItem
+
+const styles= {
+    list: {
+        padding: '0.5%',
+        marginTop: '0.5%',
+        fontSize: '1.25em',
+        fontWeight: 'bold',
+        cursor: 'pointer'
+    }
+}
