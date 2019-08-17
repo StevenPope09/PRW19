f7355d7bcfa6d2d8482b518282d2e895eb8b7c2c
Steven Pope <sjpope@student.fullsail.edu>
2019-08-17 12:27:32 -0500

    created footer component

---
+++ 02_Application/react_social_app/src/components/footer/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { FaHome } from 'react-icons/fa';
+
+const Footer = () => {
+    return (
+        <footer style={styles.footer}>
+            <FaHome /> 2019 CS. All Rights reserved.       
+            </footer>
+    )
+}
+export default Footer
+
+const styles = {
+    footer: {
+        display: 'flex',
+        flexDirection: 'row',
+        fontSize: '0.65em',
+        padding: '1%',
+        justifyContent: 'center'
+    }
+}
