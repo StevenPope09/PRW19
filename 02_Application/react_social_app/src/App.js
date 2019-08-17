a3d297feaa772743cb95daa92956886f2af8f45f
Steven Pope <sjpope@student.fullsail.edu>
2019-08-17 12:28:41 -0500

    built main functionality of site

--- 02_Application/react_social_app/src/App.js
+++ 02_Application/react_social_app/src/App.js
@@ -1,28 +1,102 @@
-import React, {Component} from 'react';
-import './App.css';
-import MyBtn from './components/buttons/MyBtn';
-import logo from '../src/images/logo.png';
-import MyForm from './components/myForm/MyForm';
-import Search from './components/search/Search';
+import React, { Component } from 'react';
+import Header from './components/header/Header'
+import MyForm from './components/myForm/MyForm'
+import Post from './components/posts/Post'
+import uuid from 'uuid/v4'
+import Search from './components/search/Search'
+import Image1 from './images/image1.jpg'
+import Footer from './components/footer/Footer'
 
 class App extends Component {
-  render() {
-    return(
-      <div>
-        <MyBtn btnText="Home" />
-        <MyBtn btnText="About" />
-        <MyBtn btnText="Contact" />
+  state = {
+    posts: [
+      { id: "1", postText: "Taking the day off of work", userName: "Jackie Smith" },
+      { id: "2", postText: "Off to the gym!", postImage: Image1, userName: "Julie Brown" },
+      { id: "3", postText: "Man school can be hard sometimes", userName: "John Smith" },
 
-        <p>
-          <img src={logo} alt="Logo" />
-        </p>
-        <MyForm addName={this.addName} />
-        <Search searchMe={this.searchMe} />
+    ],
+    search: "",
+
+    filteredPosts: []
+
+  }
+
+  onSearchChange = (e) => {
+    //console.log(e.target.value);
+    this.setState({ search: e.target.value })
+
+  }
+
+  getPosts = () => {
+    console.log(this.state);
+    // if search exists, return filter posts
+    // otherwise return original posts 
+    if (this.state.filteredPosts.length > 0) {
+      return this.state.filteredPosts
+    }
+    return this.state.posts
+
+
+  }
+
+  render() {
+    return (
+      <div style={styles.container}>
+
+        <Header pgTitle="Newton's Social Media" />
+
+        <Search searchMe={(e) => {
+          e.preventDefault()
+          //console.log(this.state.search);
+          let filteredPosts = this.state.posts.filter((post) => {
+            return post.postText.indexOf(this.state.search) >= 0
+
+          })
+
+          this.setState({ filteredPosts })
+        }} onSearchChange={this.onSearchChange} search={this.state.search} />
+
+        <MyForm onSubmit={(text, name) => {
+
+          let posts = this.state.posts;
+          posts.unshift({ postText: text, userName: name, id: uuid() })
+          this.setState({ posts })
+        }} />
+        {this.getPosts().map((post) => {
+          return (
+            <Post key={post.id} {...post} onDelete={(id) => {
+              //console.log(id);
+              let posts = this.state.posts
+              let filteredPosts = posts.filter((post) => {
+                return post.id !== id
+
+              })
+              //console.log(filteredPosts);
+              this.setState({ posts: filteredPosts })
+            }} />
+          )
+        })}
+
+        <Footer />
 
       </div>
-      
     )
   }
+
+
 }
 
+
 export default App;
+
+const styles = {
+  container: {
+    display: 'block',
+    backgroundColor: '#FFFFFF',
+    clear: 'both'
+  },
+  span: {
+    backgroundColor: 'red'
+  }
+
+}
