6713f998131a7dad98f37eb4b1d2b0aeda53d3f5
Steven Pope <sjpope@student.fullsail.edu>
2019-08-17 12:29:49 -0500

    updated form component for user input for site

--- 02_Application/react_social_app/src/components/myForm/MyForm.js
+++ 02_Application/react_social_app/src/components/myForm/MyForm.js
@@ -2,29 +2,87 @@ import React from 'react';
 import MyBtn from '../buttons/MyBtn';
 
 
-const MyForm = props => {
-    return (
-        <form
-        style={styles.MyForm}
-        onSubmit={props.addName}>
-        
-        <input
-        style={styles.input}
-        placeholder="Name" />
-        <MyBtn btnText="Add" />
-        </form>
-    )
+
+class MyForm extends React.Component {
+    state = { text: "" }
+
+    render() {
+        return (
+            <div style={styles.container}>
+                <form
+                    style={styles.myForm}>
+                    <p style={styles.mind}>What's on your mind Steven? </p>
+                    <input type='text'
+                        style={styles.input}
+                        name='text'
+                        value={this.state.text}
+                        onChange={(e) => {
+                            let text = e.target.value;
+                            this.setState({ text })
+                            console.log(text)
+                        }}
+                        placeholder="Create Post" />
+                    <p style={styles.typing}>You are typing: {this.state.text} </p>
+                    
+                    <MyBtn btnText="Post" onClick={(e) => {
+                        e.preventDefault()
+                        if (this.state.text == "") {
+                            alert("Please fill out fields before submitting post!")
+                            return
+                        }
+                        this.props.onSubmit(this.state.text, "Steven")
+                        
+                    }} />
+                </form>
+            </div>
+
+        )
+    }
+
 }
+
+// const MyForm = props => {
+//     return (
+//         <form
+//             style={styles.MyForm}
+//             onSubmit={props.addPost}>
+//             <input type='text'
+//                 style={styles.input}
+//                 name='text'
+//                 value={props.text}
+//                 onChange={props.changeMeMan}
+//                 placeholder="Name" />
+//             <p>New Item: {props.myInput}</p>
+//             <MyBtn btnText="Add" />
+//         </form>
+//     )
+// }
 
 export default MyForm;
 
-const styles= {
+const styles = {
     myForm: {
+        height: '100%',
+        width: '60%',
         margin: '1%',
         padding: '1%',
-        backgroundColor: 'rgba(163,173,194,0.5)',
+        backgroundColor: 'rgba(163,173,194,0.3)',
         color: 'rgb(163,173,194)',
         display: 'flex',
-        flexDirection:'column'
+        flexDirection: 'column',
+        marginLeft: '20%'
+    },
+    mind: {
+        color: 'black',
+        fontSize: '1.5em'
+    },
+    input: {
+        height: '75px',
+        width: '100%',
+        fontSize: '1.50em'
+    },
+    typing: {
+        color: 'rgba(0,0,0,.5)'
     }
+    
 }
