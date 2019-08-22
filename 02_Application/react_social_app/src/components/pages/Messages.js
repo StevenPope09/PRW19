e5d40dbeea75e74b96ccd196ca00f6be9bf70635
Steven Pope <sjpope@student.fullsail.edu>
2019-08-22 17:26:29 -0500

    added pages for navigation to navigate to

---
+++ 02_Application/react_social_app/src/components/pages/Messages.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Nav from '../nav/Nav'
+
+
+class Messages extends React.Component {
+    render() {
+        return (
+
+            <section>
+                <p>Testing Messages</p>
+            </section>
+
+        )
+    }
+}
+
+export default Messages
---
+++ 02_Application/react_social_app/src/components/pages/Watch.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Nav from '../nav/Nav'
+
+
+class Watch extends React.Component {
+    render() {
+        return (
+            
+                <section>
+                    <p>Testing Watch</p>
+                </section>
+            
+        )
+    }
+}
+
+export default Watch
