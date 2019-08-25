4d66c49baa0c0cb2424fedb8124e7eb0ee40fb11
Steven Pope <sjpope@student.fullsail.edu>
2019-08-24 21:05:34 -0500

    reworked structure of site

--- 02_Application/react_social_app/src/App.js
+++ 02_Application/react_social_app/src/App.js
@@ -1,110 +1,25 @@
 import React, { Component } from 'react';
 import Header from './components/header/Header'
-import MyForm from './components/myForm/MyForm'
-import Post from './components/posts/Post'
-import uuid from 'uuid/v4'
-import Search from './components/search/Search'
-import Image1 from './images/image1.jpg'
 import Footer from './components/footer/Footer'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Main from './components/main/Main'
+import NewsFeed from './components/pages/NewsFeed';
+import Messages from './components/pages/Messages'
+import Watch from './components/pages/Watch';
+import Image from './images/image1.jpg'
 
 class App extends Component {
-  state = {
-    posts: [
-      { id: "1", postText: "Taking the day off of work", userName: "Jackie Smith" },
-      { id: "2", postText: "Off to the gym!", postImage: Image1, userName: "Julie Brown" },
-      { id: "3", postText: "Man school can be hard sometimes", userName: "John Smith" },
-
-    ],
-    search: "",
-
-    filteredPosts: []
-
-  }
-
-  componentDidMount() {
-    if (localStorage.getItem('pList')) {
-      let pList = JSON.parse(localStorage.getItem('pList'))
-      this.setState({ posts: pList })
-    }
-  }
-
-  onSearchChange = (e) => {
-    //console.log(e.target.value);
-    this.setState({ search: e.target.value })
-
-  }
-
-  getPosts = () => {
-    console.log(this.state);
-
-    // if search exists, return filter posts
-    // otherwise return original posts 
-    if (this.state.filteredPosts.length > 0) {
-      return this.state.filteredPosts
-
-    }
-    return this.state.posts
-
-
-  }
-
-  //Local Storage 
-  // let pList = [...runInThisContext.state.posts]
-  //             pList.push({ pList: this.state.post })
-  //             this.setState({ pList })
-  //             localStorage.setItem('pList', JSON.stringify(pList))
 
   render() {
     return (
       <Router>
 
         <div style={styles.container}>
-          <Main />
+
           <Header pgTitle="Neutron Gram" />
-
-          <Search searchMe={(e) => {
-            e.preventDefault()
-            //console.log(this.state.search);
-            if (this.state.search == "") {
-              alert("Please fill this field out before you continue")
-              return
-            }
-
-            let filteredPosts = this.state.posts.filter((post) => {
-              return post.userName.indexOf(this.state.search) >= 0
-
-            })
-
-            this.setState({ filteredPosts })
-
-
-          }} onSearchChange={this.onSearchChange} search={this.state.search} />
-
-          <MyForm onSubmit={(text, name) => {
-
-
-            let posts = this.state.posts;
-            posts.unshift({ postText: text, userName: name, id: uuid() })
-            this.setState({ posts })
-          }} />
-          {this.getPosts().map((post) => {
-            return (
-              <Post key={post.id} {...post} onDelete={(id) => {
-                //console.log(id);
-                let posts = this.state.posts
-                let filteredPosts = posts.filter((post) => {
-                  return post.id !== id
-
-                })
-                //console.log(filteredPosts);
-                this.setState({ posts: filteredPosts })
-
-              }} />
-
-            )
-          })}
+          
+          <Route exact path='/News-Feed' component={NewsFeed} />
+          <Route exact path='/Messages' component={Messages} />
+          <Route exact path='/Watch' component={Watch} />
 
           <Footer />
 
@@ -119,7 +34,8 @@ export default App;
 const styles = {
   container: {
     display: 'block',
-    backgroundColor: 'white',
+    background: 'white',
+    backgroundSize: '50% 100%'
 
 
   },
